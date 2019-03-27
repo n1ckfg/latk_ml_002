c6df9c618df30b9de0608a93294bea2e74c4e33f
Nick Fox-Gieg <nick@fox-gieg.com>
2019-03-27 17:48:50 -0400

    add submodule

--- latkml.py
+++ latkml.py
@@ -4,3 +4,7 @@ sys.path.insert(1, './pix2pix-tensorflow')
 
 import latk
 import pix2pix
+import PIL.Image as Image # https://gist.github.com/n1ckfg/58b5425a1b81aa3c60c3d3af7703eb3b
+import svgpathtools as svg # https://github.com/mathandy/svgpathtools
+
+paths, attributes, svg_attributes = svg.svg2paths2('test.svg')
