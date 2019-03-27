261f810b7ecd6fefbdef9dc35363040ac1dd51c2
n1ckfg <nick@fox-gieg.com>
2019-03-27 19:54:25 -0400

    Update latkml.py

--- latkml.py
+++ latkml.py
@@ -1,6 +1,16 @@
 import sys
+import os
+import platform
+
 sys.path.insert(0, './latk.py')
 sys.path.insert(1, './pix2pix-tensorflow')
 
 import latk
 import pix2pix
+
+osName = platform.system()
+autotracePath = "autotrace" # Linux
+if (osName == "Windows"):
+	autotracePath = "C:\\Program Files\\AutoTrace\\autotrace.exe"
+elif (osName == "Mac"):
+	autotracePath == "/Applications/AutoTrace.app/Contents/MacOs/AutoTrace"
