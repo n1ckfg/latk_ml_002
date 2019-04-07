e2e91f8f6968ecd7e3e220fc0e035e21352027f4
n1ckfg <nick@fox-gieg.com>
2019-04-07 16:33:05 -0400

    Update test.py

--- tools/svg_tester/test.py
+++ tools/svg_tester/test.py
@@ -16,6 +16,9 @@ def getDistance2D(v1, v2):
     v2 = (v2[0], v2[1], 0)
     return sqrt((v1[0] - v2[0])**2 + (v1[1] - v2[1])**2 + (v1[2] - v2[2])**2)
 
+def getDistance(v1, v2):
+    return sqrt((v1[0] - v2[0])**2 + (v1[1] - v2[1])**2 + (v1[2] - v2[2])**2)
+
 def getPathLength(path):
     firstPoint = getCoordFromPathPoint(path.point(0))
     lastPoint = getCoordFromPathPoint(path.point(1))
@@ -25,14 +28,15 @@ la = Latk(init=True)
 paths, attr = svg2paths("test.svg")
 
 for path in paths:
-    numPoints = getPathLength(path) * 10
+    numPoints = getPathLength(path)
     numRange = int(numPoints)
-    coords = []
-    for i in range(numRange):
-        pt = path.point(i/(numPoints-1))
-        point = getCoordFromPathPoint(pt)
-        coords.append((point[0], point[1], 0))
-    la.setCoords(coords)
+    if (numRange > 1):
+        coords = []
+        for i in range(numRange):
+            pt = path.point(i/(numPoints-1))
+            point = getCoordFromPathPoint(pt)
+            coords.append((point[0]/100.0, point[1]/100.0, 0))
+        la.setCoords(coords)
 
-la.clean()
+la.clean(epsilon=0.0001)
 la.write("test.latk")
