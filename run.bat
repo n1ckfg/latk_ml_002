600e94d8913c60dfb144b6e54918c062f01fa7d2
n1ckfg <nick@fox-gieg.com>
2019-05-15 14:46:08 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,6 +4,6 @@ rem https://serverfault.com/questions/95686/change-current-directory-to-the-batc
 cd /D %~dp0
 
 rem file, fps, use depth for contour, min path points, num colors, error threshold, line threshold, line reversion threshold
-python latkml.py -- %1 1 True 6 2 100 10 20
+python latkml.py -- %1 1 True 4 2 100 10 20
 
 @pause
