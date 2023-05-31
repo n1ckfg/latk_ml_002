53ec911fd864b8290dd896278059fbf9744555a7
Nick Fox-Gieg <nick@fox-gieg.com>
2023-05-31 18:34:07 -0400

    ui

---
+++ __init__.py
@@ -0,0 +1,125 @@
+bl_info = {
+    "name": "latk-ml-002", 
+    "author": "Nick Fox-Gieg",
+	"version": (0, 0, 1),
+	"blender": (3, 0, 0),
+    "description": "Generate brushstrokes from a mesh using pix2pix",
+    "category": "Animation"
+}
+
+import bpy
+from bpy.types import Operator, AddonPreferences
+from bpy.props import (BoolProperty, FloatProperty, StringProperty, IntProperty, PointerProperty, EnumProperty)
+from bpy_extras.io_utils import (ImportHelper, ExportHelper)
+
+class latkml002Preferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    '''
+    extraFormats_AfterEffects: bpy.props.BoolProperty(
+        name = 'After Effects JSX',
+        description = "After Effects JSX export",
+        default = False
+    )
+	'''
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.label(text="none")
+        #row = layout.row()
+        #row.prop(self, "extraFormats_Painter")
+
+# This is needed to display the preferences menu
+# https://docs.blender.org/api/current/bpy.types.AddonPreferences.html
+class OBJECT_OT_latkml002_prefs(Operator):
+    """Display example preferences"""
+    bl_idname = "object.steve" #+ __name__
+    bl_label = "latkml002 Preferences"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        preferences = context.preferences
+        addon_prefs = preferences.addons[__name__].preferences
+        return {'FINISHED'}
+
+class latkml002Properties(bpy.types.PropertyGroup):
+    """Properties for latkml002"""
+    bl_idname = "GREASE_PENCIL_PT_latkml002Properties"
+
+    '''
+    bakeMesh: BoolProperty(
+        name="Bake",
+        description="Off: major speedup if you're staying in Blender. On: slower but keeps everything exportable",
+        default=True
+    )
+	'''
+
+class latkml002_Button_AllFrames(bpy.types.Operator):
+    """Operate on all frames"""
+    bl_idname = "latkml002_button.allframes"
+    bl_label = "All Frames"
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        # function goes here
+        pass
+        return {'FINISHED'}
+
+class latkml002_Button_SingleFrame(bpy.types.Operator):
+    """Operate on a single frame"""
+    bl_idname = "latkml002_button.singleframe"
+    bl_label = "Single Frame"
+    bl_options = {'UNDO'}
+    
+    def execute(self, context):
+        # function goes here
+        pass
+        return {'FINISHED'}
+
+# https://blender.stackexchange.com/questions/167862/how-to-create-a-button-on-the-n-panel
+class latkml002Properties_Panel(bpy.types.Panel):
+    """Creates a Panel in the 3D View context"""
+    bl_idname = "GREASE_PENCIL_PT_latkml002PropertiesPanel"
+    bl_space_type = 'VIEW_3D'
+    bl_label = "latk-ml-002"
+    bl_category = "Latk"
+    bl_region_type = 'UI'
+    #bl_context = "objectmode" # "mesh_edit"
+
+    #def draw_header(self, context):
+        #self.layout.prop(context.scene.freestyle_gpencil_export, "enable_latk", text="")
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+        latkml002 = scene.latkml002_settings
+
+        row = layout.row()
+        row.operator("latkml002_button.allframes")
+        row.operator("latkml002_button.singleframe")
+        #row.prop(latkml002, "material_shader_mode")
+
+classes = (
+    OBJECT_OT_latkml002_prefs,
+    latkml002Preferences,
+    latkml002Properties,
+    latkml002Properties_Panel,
+	latkml002_Button_AllFrames,
+	latkml002_Button_SingleFrame
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)   
+    bpy.types.Scene.latkml002_settings = bpy.props.PointerProperty(type=latkml002Properties)
+
+def unregister():
+    del bpy.types.Scene.latkml002_settings
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+
+if __name__ == "__main__":
+    register()
+
